30c65e3990e4519c599caeeb4e2ee0d8b85fc15c
Taiyo200 <petrusvlad01@gmail.com>
2025-06-09 14:31:02 +0300

    Added the usage of the free gemini model and CLI arguments

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+load_dotenv("env/gemini_api.env")
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+if sys.argv is not None and len(sys.argv) > 1:
+    prompt = sys.argv[1]
+else:
+    raise ValueError("Please provide a prompt as a command line argument.")
+
+
+answer = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+print(answer.text)
+
+token_count = answer.usage_metadata.prompt_token_count 
+candidates_count = answer.usage_metadata.candidates_token_count
+
+print(f"Prompt tokens: {token_count}\nResponse tokens: {candidates_count}")
