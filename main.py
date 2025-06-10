add803ddcc4a8ed05c458cdf66da764a2c50fd3d
Taiyo200 <petrusvlad01@gmail.com>
2025-06-10 15:22:31 +0300

    Added verbose argument check at runtime

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ if sys.argv is not None and len(sys.argv) > 1:
 else:
     raise ValueError("Please provide a prompt as a command line argument.")
 
-
 answer = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
-print(answer.text)
 
 token_count = answer.usage_metadata.prompt_token_count 
 candidates_count = answer.usage_metadata.candidates_token_count
 
-print(f"Prompt tokens: {token_count}\nResponse tokens: {candidates_count}")
+if "--verbose" in sys.argv or "-v" in sys.argv:
+    print(f"User prompt:\n{prompt}\n\nAnswer:\n{answer.text}\n\nPrompt tokens: {token_count}\n\nResponse tokens: {candidates_count}")
+
+else:
+    print(answer.text)
